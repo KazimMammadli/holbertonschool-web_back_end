a5bcecb2a43d58459e1cd629c2eb7e78ef68ae5e
Kazim Mammadli <kazim.memmedli.005@gmail.com>
2025-07-11 22:37:29 +0400

    12.task

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+client = MongoClient('mongodb://localhost:27017/')
+db = client.logs
+collection = db.nginx
+print(f"{collection.count_documents()} logs\nMethods:")
+method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for element in method:
+    print(f"\tmethod {element}: \
+          {collection.count_documents({"method": element})}")
+print(f"{collection.count_documents({{"method": "GET", "path": "/status"}})}")
